ea9bb5c13e9b3e6a38dddaac386386e24d073fd1
mohammedqasim91 <mohamadqasim888@gmail.com>
2023-08-12 06:11:39 +0200

    first commit 14 changes

---
+++ vue-project/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import NavBar from './components/NavBar.vue';
+import Footer from './components/Footer.vue';
+
+import { ref } from 'vue';
+const newCss = ref("app-new");
+
+function toggleTheme() {
+  newCss.value = newCss.value === "app-new" ? "" : "app-new";
+}
+</script>
+
+<template>
+  <section :class="newCss">
+    <NavBar></NavBar>
+    <Footer></Footer>
+    <button @click="toggleTheme">Change Theme</button>
+  </section>
+  
+</template>
+
+<style>
+.app-new {
+    background-color: green;
+    color: white;
+}
+</style>
